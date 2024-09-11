570ea4c3a7740ffec17946b238f7cd5b90bd2935
adffdfewqefw <bbeekhw@naver.com>
2024-09-11 09:24:10 +0900

    counter-thread 예제 수정

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -20,8 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 public class App 
 {
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws InterruptedException {
 
         //TODO#1 shardCounter 객체를 0으로 초기화 합니다.
         SharedCounter sharedCounter = new SharedCounter(0);
@@ -33,22 +32,25 @@ public class App
         Thread threadA = new Thread(counterIncreaseHandler);    //Thread에 syn 효과 삽입.
 
         //TODO#4 threadA의 thread name을 "thread-A"로 설정 합니다.
-
+        threadA.setName("thread-A");
         //TODO#5 threadA를 시작 합니다.
-
+        threadA.start();
 
         //TODO#6 counterIncreaseHandler를 이용해서 threadB를 생성 합니다.
-        Thread threadB = null;
+        Thread threadB = new Thread(counterIncreaseHandler);
 
         //TODO#7 threadB의 name을 'thread-B' 로 설정 합니다.
-
+        threadB.setName("thread-B");
         //TODO#8 threadB를 시작 합니다.
-
+        threadB.start();
         //TODO#9 main thread가 실행 후 20초 후 threadA, threadB 종료될 수 있도록 interrupt 발생 시킵니다.
-
+        Thread.sleep(20000);
+        threadA.interrupt();
+        threadB.interrupt();
 
         //TODO#10 main Thread는 threadA와 threadB의 상태가 terminated가 될 때 까지 대기 합니다. 즉 threadA, threadB가 종료될 때 까지 대기(양보) 합니다.
-
+        threadB.join();
+        threadA.join();
 
         log.debug("System exit!");
     }
